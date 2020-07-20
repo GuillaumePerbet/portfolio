e60239df15c5fe0be5e6038ca75a43d6abbad6bb
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-20 14:43:44 +0200

    commit before push on server

--- ajax/contact-form.php
+++ ajax/contact-form.php
@@ -1,6 +1,6 @@
 <?php
 
 var_dump($_POST);
-// $message = str_replace("\n.", "\n..", $_POST['message']);
+$message = str_replace("\n.", "\n..", $_POST['message']);
 
 var_dump(mail('perbet.dev@gmail.com', $_POST['object'], $message,"From: ".$_POST['email']));
