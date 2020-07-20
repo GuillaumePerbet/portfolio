980bbcdd8278566fbd603f2f93b85e04d21a945b
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-20 11:12:43 +0200

    menu activation and modal format

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//SMOOTH ANCHOR LINK SCROLL
-let anchorlinks = document.querySelectorAll('a[href^="#"]');
+//SMOOTH ANCHOR LINK SCROLL__________________________________________
+const anchorlinks = document.querySelectorAll('a[href^="#"]');
 for (let item of anchorlinks) {
     item.addEventListener('click', (e)=> {
         let hashval = item.getAttribute('href');
@@ -12,3 +12,24 @@ for (let item of anchorlinks) {
         e.preventDefault();
     });
 }
+
+//MENU DISPLAY_________________________________________________________
+const menuBtn = document.getElementById('top-bar-menu-btn');
+menuBtn.addEventListener('click',()=>{
+    //hide top bar
+    const topBar = document.getElementById('top-bar');
+    topBar.classList.add('d-none');
+    //show menu
+    const menu = document.getElementById('menu');
+    menu.classList.remove('d-none');
+});
+
+const closeBtn = document.getElementById('menu-close');
+closeBtn.addEventListener('click',()=>{
+    //hide menu
+    const menu = document.getElementById('menu');
+    menu.classList.add('d-none');
+    //show top bar
+    const topBar = document.getElementById('top-bar');
+    topBar.classList.remove('d-none');
+});
