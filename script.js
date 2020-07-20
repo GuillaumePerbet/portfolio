b9a6102df7021a3e64633dfc8305c407f4ab9d72
Guillaume Perbet <perbet.dev@gmail.com>
2020-07-20 14:38:07 +0200

    enable click under transparent element

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-//SMOOTH ANCHOR LINK SCROLL__________________________________________
+//DISPLAY FUNCTIONS
+function showMenu(){
+    const menu = document.getElementById('menu');
+    menu.classList.remove('d-none');
+}
+
+function hideMenu(){
+    const menu = document.getElementById('menu');
+    menu.classList.add('d-none');
+}
+
+function showTopBar(){
+    const topBar = document.getElementById('top-bar');
+    topBar.classList.remove('d-none');
+}
+
+function hideTopBar(){
+    const topBar = document.getElementById('top-bar');
+    topBar.classList.add('d-none');
+}
+
+//SMOOTH SCROLL ANCHOR LINK__________________________________________
 const anchorlinks = document.querySelectorAll('a[href^="#"]');
 for (let item of anchorlinks) {
     item.addEventListener('click', (e)=> {
+        //for menu links
+        hideMenu();
+        showTopBar();
         let hashval = item.getAttribute('href');
         let target = document.querySelector(hashval);
         target.scrollIntoView({
@@ -13,28 +37,21 @@ for (let item of anchorlinks) {
     });
 }
 
-//MENU DISPLAY_________________________________________________________
+//OPEN MENU_________________________________________________________
 const menuBtn = document.getElementById('top-bar-menu-btn');
 menuBtn.addEventListener('click',()=>{
-    //hide top bar
-    const topBar = document.getElementById('top-bar');
-    topBar.classList.add('d-none');
-    //show menu
-    const menu = document.getElementById('menu');
-    menu.classList.remove('d-none');
+    hideTopBar();
+    showMenu();
 });
 
+//CLOSE MENU_____________________________________________________________
 const closeBtn = document.getElementById('menu-close');
 closeBtn.addEventListener('click',()=>{
-    //hide menu
-    const menu = document.getElementById('menu');
-    menu.classList.add('d-none');
-    //show top bar
-    const topBar = document.getElementById('top-bar');
-    topBar.classList.remove('d-none');
+    hideMenu();
+    showTopBar();
 });
 
-//CONTACT FORM SUBMIT___________________________________________________
+//SUBMIT CONTACT FORM___________________________________________________
 const contactForm = document.querySelector("#contact form");
 contactForm.addEventListener('submit',(e)=>{
     e.preventDefault();
